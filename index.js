c9f313da4801a2c9c15b67be2aeed459fe669c44
Michael Jeung <michaelj229@gmail.com>
2023-02-20 16:34:42 -0500

    code along

--- index.js
+++ index.js
@@ -1,1 +1,78 @@
 // Follow along with the examples here
+// function doNothing() {}
+
+// function sayHello() {
+//     console.log("Hello!");
+// }
+// sayHello();
+
+// function sayHelloToGuadalupe() {
+//     console.log("Hello Guadalupe!");
+// }
+// sayHelloToGuadalupe();
+
+// function sayHelloToLiz() {
+//     console.log("Hello Liz!");
+// }
+// sayHelloToLiz();
+
+// function sayHellotoSamip() {
+//     console.log("Hello Samip!");
+// }
+// sayHellotoSamip();
+
+// sayHelloToGuadalupe();
+// sayHelloToLiz();
+// sayHellotoSamip();
+
+// function sayHelloTo(firstName) {
+//     console.log(`Hello, ${firstName}!`);
+// }
+// sayHelloTo("Guadalupe");
+// sayHelloTo("Jane");
+// sayHelloTo("R2-D2");
+// sayHelloTo(1);
+
+// function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+// }
+// say("Goodbye", "Julio");
+// say("Julio", "hello");
+
+// function say(greeting, firstName) {
+//     console.log("first name: ", firstName);
+//     console.log("greeting: ", greeting);
+//     console.log(`${greeting}, ${firstName}!`);
+// }
+// say("Julio", "hello");
+
+// function add(x, y) {
+//     return x + y;
+// }
+// console.log(add(80, 9000));
+
+// function say(greeting, firstName) {
+//     return `${greeting}, ${firstName}!`;
+// }
+// console.log(say("Hello", "Liz"));
+
+// function say(greeting, firstName) {
+//     console.log(`${greeting}, ${firstName}!`);
+// }
+// console.log(say("Hello", "Liz"));
+
+// function add(x, y) {
+//     x + y;
+// }
+// console.log(add(1, 2));
+
+// function say(greeting, firstName) {
+//     return `${greeting}, ${firstName}!`;
+//     console.log("I was called!");
+// }
+// console.log(say("Howdy", "partner"));
+
+// function say(size, drink) {
+//     return `Hello, what would you like to drink today? I would like a ${size} ${drink} please!`;
+// }
+// console.log(say("large", "coffee"));
